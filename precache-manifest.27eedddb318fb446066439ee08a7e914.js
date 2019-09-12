d45fe0d9453d6ee4e2ec3782f1868736f588438c
farshid inanloo <farshidinanloo73@gmail.com>
2019-09-12 11:57:50 +0430

    Updates

---
+++ precache-manifest.27eedddb318fb446066439ee08a7e914.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eddfe742a7d8c5ddf6822425042e4b2",
+    "url": "/React-Iran-Tour/index.html"
+  },
+  {
+    "revision": "dd54cf8339d85e214ff0",
+    "url": "/React-Iran-Tour/static/css/main.369df3e9.chunk.css"
+  },
+  {
+    "revision": "c77add7b5c9ea35197b5",
+    "url": "/React-Iran-Tour/static/js/2.c0210f73.chunk.js"
+  },
+  {
+    "revision": "dd54cf8339d85e214ff0",
+    "url": "/React-Iran-Tour/static/js/main.667266e4.chunk.js"
+  },
+  {
+    "revision": "a1d77317d44125bfaee5",
+    "url": "/React-Iran-Tour/static/js/runtime~main.ad50440d.js"
+  },
+  {
+    "revision": "2ecd45d66c9dc68aee3f8601657c9ec7",
+    "url": "/React-Iran-Tour/static/media/Persepolis.2ecd45d6.jpg"
+  },
+  {
+    "revision": "f062f388b35f2fe59a2864d9ff193632",
+    "url": "/React-Iran-Tour/static/media/cheetah.f062f388.jpg"
+  },
+  {
+    "revision": "466a0955c634871ebf26a89cbb9c4321",
+    "url": "/React-Iran-Tour/static/media/guideOne.466a0955.jpeg"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/React-Iran-Tour/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "f4b66255dae996236c920e86b5ee32cd",
+    "url": "/React-Iran-Tour/static/media/nat-1.f4b66255.jpg"
+  },
+  {
+    "revision": "cb31a1e77914b880822ff938f937120a",
+    "url": "/React-Iran-Tour/static/media/nat-10.cb31a1e7.jpg"
+  },
+  {
+    "revision": "0eb40f1357e4ab0756da4f24d3e9ba48",
+    "url": "/React-Iran-Tour/static/media/nat-2.0eb40f13.jpg"
+  },
+  {
+    "revision": "ea3feab991ee4ef8136a7a10d8467223",
+    "url": "/React-Iran-Tour/static/media/nat-3.ea3feab9.jpg"
+  },
+  {
+    "revision": "1f309de86c412261b89406a98e7ce1f4",
+    "url": "/React-Iran-Tour/static/media/nat-4.1f309de8.jpg"
+  },
+  {
+    "revision": "b12b00369a5709229f3a616850c2cf18",
+    "url": "/React-Iran-Tour/static/media/nat-6.b12b0036.jpg"
+  },
+  {
+    "revision": "100bc817eccb3a03606786e0103e1ea2",
+    "url": "/React-Iran-Tour/static/media/nat-7.100bc817.jpg"
+  },
+  {
+    "revision": "f26015f94c5baf74777af4605c2600ee",
+    "url": "/React-Iran-Tour/static/media/p1.f26015f9.png"
+  },
+  {
+    "revision": "11cbfd4e666f89bfcaefa6659431186d",
+    "url": "/React-Iran-Tour/static/media/p2.11cbfd4e.png"
+  },
+  {
+    "revision": "56aa413b681ad54345d9572713f4a527",
+    "url": "/React-Iran-Tour/static/media/p3.56aa413b.png"
+  },
+  {
+    "revision": "54ceb0f1579c6b3a1bf105f8e666425f",
+    "url": "/React-Iran-Tour/static/media/video.54ceb0f1.mp4"
+  }
+]);
